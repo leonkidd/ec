3e687520395eee3b97eade4ec3b22ac4bbac0ce2
Leon <304552328@qq.com>
2013-11-14 11:55:23 +0800

    Temp

--- src/main/java/cn/heroes/ec/Main.java
+++ src/main/java/cn/heroes/ec/Main.java
@@ -1,17 +1,20 @@
 package cn.heroes.ec;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Main {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
 	// Client_ID, Socket
 	private static final Map<String, Socket> clients = new HashMap<String, Socket>();
@@ -25,12 +28,53 @@ public class Main {
 				while (true) {
 					try {
 						// 同个客户端再次联接怎么办
-						Socket client = ss.accept();
-						
+						final Socket client = ss.accept();
+
 						InetAddress ia = client.getInetAddress();
 						String ip = ia.getHostAddress();
 						int port = client.getPort();
+
 						clients.put(ip, client);
+
+						new Thread() {
+							@Override
+							public void run() {
+								try {
+									final InputStream is = client.getInputStream();
+									final OutputStream os = client.getOutputStream();
+									
+									Socket socket = new Socket("192.168.200.7", 23);
+									final OutputStream _os = socket.getOutputStream();
+									final InputStream _is = socket.getInputStream();
+
+									new Thread() {
+										@Override
+										public void run() {
+											try {
+												IOUtils.copy(is, _os);
+											} catch (IOException e) {
+												e.printStackTrace();
+											}
+											
+										}
+									}.start();
+									new Thread() {
+										@Override
+										public void run() {
+											try {
+												IOUtils.copy(_is, os);
+											} catch (IOException e) {
+												e.printStackTrace();
+											}
+											
+										}
+									}.start();
+								} catch (IOException e) {
+									e.printStackTrace();
+								}
+								
+							}
+						}.start();
 						logger.info("新客户{}[{}]连接成功", ip, port);
 					} catch (IOException e) {
 						e.printStackTrace();
@@ -38,8 +82,7 @@ public class Main {
 				}
 			}
 		}.start();
-		
-		
+
 	}
 
 }
